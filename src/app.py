75f3002d51b993d045cc308132f85fa8558969d6
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2025-01-09 17:32:44 -0800

    formatted csv file

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def index():
 
 @app.route('/data')
 def data():
-    df = pd.read_csv('data/oura_2024-10-28_2024-12-28_trends.csv')
+    df = pd.read_csv('data/oura_2024-10-28_2024-12-28_trends.csv', skipinitialspace=True)
     df['date'] = pd.to_datetime(df['date']).dt.date
     scores = df[['date', 'Resting Heart Rate Score']].to_dict(orient='records')
     return jsonify(scores)
